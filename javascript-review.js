760fd5a6e0c7eea5c67a6042ad58d1ed1e6bb40d
Ji Shan <uug54332017@gmail.com>
2020-06-15 11:36:47 +1000

    up to Q21

--- javascript-review.js
+++ javascript-review.js
@@ -5,31 +5,30 @@
 // Create an object that has four properties. One property should be set so that the value is a number, another property set to a string, the third to an array, and the fourth to a function. This function should simply console.log the value of the first property.
 
 const movie = {
-  title: 'The matrix',
-  year: '1999',
-  cast: ['Keanu Reeves', 'Carrie-Anne Moss', 'Laurence FishBurne'],
-  showTitle: function(){
-    console.log(this.title)
-  }
-}
+  title: "The matrix",
+  year: "1999",
+  cast: ["Keanu Reeves", "Carrie-Anne Moss", "Laurence FishBurne"],
+  showTitle: function () {
+    console.log(this.title);
+  },
+};
 
 // movie.showTitle();
 
 // 2.
 
-// Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’. 
+// Write a constructor function named Car that takes one argument. It should use that argument to set the brand attribute of the car object. There should be a second attribute that stores the value for colour but this should be explicitly set to ‘red’.
 
-// Note that a constructor function is the old school syntax that was used in javascript before the sugar class syntax came along in ES6. Don't use the sugar syntax for this question! 
+// Note that a constructor function is the old school syntax that was used in javascript before the sugar class syntax came along in ES6. Don't use the sugar syntax for this question!
 
-function Car(brand, color = 'red'){
+function Car(brand, color = "red") {
   this.brand = brand;
   this.color = color;
 }
 
-const c = new Car('tesla')
+const c = new Car("tesla");
 // console.log(c.color)
 // console.log(c.brand)
-
 
 // 3.
 
@@ -37,7 +36,7 @@ const c = new Car('tesla')
 
 const a = 1;
 function show() {
-  console.log(a)
+  console.log(a);
 }
 
 // show();
@@ -47,16 +46,15 @@ function show() {
 // What two ways can be used to access the properties of objects in JS? Give examples of both (although assume the objects have been defined).
 
 // dot notation
-console.log(movie.year);
+// console.log(movie.year);
 // bracket notation
-console.log(movie['year']);
-
+// console.log(movie["year"]);
 
 // 5.
 
 // What is JSON? Be clear but concise.
 
-// JavaScript Object Notation => a lightweight format for sorting and transporting data. 
+// JavaScript Object Notation => a lightweight format for sorting and transporting data.
 
 // 6.
 
@@ -64,37 +62,34 @@ console.log(movie['year']);
 
 // A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete action.
 
-
 // 7.
 
 // Define a function with that takes two number arguments and a callback. The function will add the two numbers and pass them to the callback as an argument. Now call this function and in the callback simply console.log out the argument that has been passed through.
 
+function showAddition(num1, num2, cb) {
+  let answer = num1 + num2;
 
-function showAddition(num1, num2, cb){
- let answer =  num1 + num2;
-
- if (cb) {
-   return cb(answer);
- }
- return answer;
+  if (cb) {
+    return cb(answer);
+  }
+  return answer;
 }
 
 function display(answer) {
   return answer;
 }
 
-const b = showAddition(1, 1, display() )
-console.log(b)
+const b = showAddition(1, 1, display());
+// console.log(b);
 
 // 8.
 // Why do we use promises?
 
 // use promises to fix callback limitations (callback hell)
 
-
 // 9.
 // What are the two functions at our disposal if we are defining our own promise?
-// Hint: They're in the new Promise callback function as parameters. 
+// Hint: They're in the new Promise callback function as parameters.
 
 // resolve and reject
 
@@ -102,24 +97,21 @@ console.log(b)
 // What's the different between the rest and spread syntax?
 
 // Rest operator is used to form and array with any number of arguments.
-//Spread operator is to show the values from an array which is opposite to the rest operator. 
+//Spread operator is to show the values from an array which is opposite to the rest operator.
 
-
-// 11. 
+// 11.
 // Define a function myFunc(), it should take 3 number arguments, use the rest syntax in the myFunc parameters and console.log out the value generated from the rest.
 
-function myFunc(...numbers) {
-    console.log(numbers)
-}
+// function myFunc(...numbers) {
+//   console.log(numbers);
+// }
 
 // myFunc(1,2,3);
-
 
 // 12.
 // What is a javascript package manager? Name the 2 main package managers
 
 // Npm and Yarn
-
 
 // 13.
 // What is a package.json? What is it similar to when comparing it to ruby?
@@ -128,99 +120,122 @@ function myFunc(...numbers) {
 
 // It is similar to a gemspec which describes the functionality of the package and lists all the dependencies.
 
-
 // 14.
-// a. 
+// a.
 // run the following command
 // npm init -y
 
-// b. 
+// b.
 // Install the package that allows us to get user input in node
-// Store the result of the user input in a variable name then console.log the value of the variable on the subsequent line 
+// Store the result of the user input in a variable name then console.log the value of the variable on the subsequent line
 
 // let readlineSync = require('readline-sync');
 // var userName = readlineSync.question(' Hello, what\'s your name? ');
 // console.log('Welcome ' + userName + '^^');
 
-
 // 15.
 
 // a.
-// Define a function called addNum with that takes two number arguments. In this function simply return the addition of these numbers. 
+// Define a function called addNum with that takes two number arguments. In this function simply return the addition of these numbers.
 
-function addNum(num1 , num2) {
+function addNum(num1, num2) {
   return num1 + num2;
 }
 
 // b.
-// Write a second function called numsPlusFunct that takes three arguments, two numbers and a function. Inside numsPlusFunct call the function that is passed as an argument, and pass the two number arguments to this function. numsPlusFunct will return an object where the first property has the value returned from that function call, and the second property is a string. 
+// Write a second function called numsPlusFunct that takes three arguments, two numbers and a function. Inside numsPlusFunct call the function that is passed as an argument, and pass the two number arguments to this function. numsPlusFunct will return an object where the first property has the value returned from that function call, and the second property is a string.
 
 function numsPlusFunct(num1, num2, cb) {
-  const string = 'The final number is';
-  return  string + cb(num1,num2);
+  const string = "The final number is";
+  return string + cb(num1, num2);
 }
 
-
 // c.
-// You have now made two functions. Call the numsPlusFunct and pass addNum as the appropriate argument. 
+// You have now made two functions. Call the numsPlusFunct and pass addNum as the appropriate argument.
 
 // console.log(numsPlusFunct(1,2, addNum))
-
 
 // 16.
 // Define a .txt file and put this text into it => "hello world"
 // Using the fs module in node read this text from the file into this program and console.log it
-const fs = require('fs');
-let sample = fs.createWriteStream('sample.txt');
+const fs = require("fs");
+let sample = fs.createWriteStream("sample.txt");
 
 sample.write("hello world");
 sample.end();
 
+// fs.appendFile('node.txt', 'Hello content!', function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+
 
 // 17.
-// What is the difference between synchronous and asynchronous code? Name one way that JS handles asynchronous code. 
+// What is the difference between synchronous and asynchronous code? Name one way that JS handles asynchronous code.
 
 //Asynchromous programming is a method of programming that allows more than one task to be executed at the same time.
 
 // async is added to functions to tell them to return a promise rather than returnning the value directly.
 
-
 // 18.
 // What is fetch and how does it relate to AJAX? Give an example of how you would use it. What does fetch return? Give a very basic example of fetch.
 
-// 19.
+//Fetch is a Web API and works on broswers. This API was developed to make AJAx requests easier. and it returns a promise. (Asynchronous JavaScript and XML = AJAX)
+const fetch = require('node-fetch');
 
+// fetch("https://api.chucknorris.io/jokes/random")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// 19.
 // A JS object looks like this: const southernField = { location: “upper”, crop: “sorghum”, watered: false }. Use destructuring to store the value of watered in a variable.
 
-// 20.
+const southernField = { 
+  location: "upper", 
+  crop: "sorghum", 
+  watered: false 
+}; 
 
+const status = southernField.watered;
+// console.log(status);
+
+const {watered} = southernField;
+// console.log(watered);
+
+
+// 20.
 // a.
 // Uncomment the code below.
 
-// let newNum = 1
+let newNum = 1
 
-// const myFunc = (num, callback) => {
-//   newNum *= num
-//   callback(newNum)
-// }
+const myFunc = (num, callback) => {
+  newNum *= num
+  callback(newNum)
+}
 
-// myFunc(10, (sum) => {
-//   console.log(sum)
-// })
+myFunc(10, (sum) => {
+  console.log(sum)
+})
 
 // b.
 // Explain in your own words how this code works. For example you could start with something like:
 
-// "Firstly, the letNum variable is initialized and receives the value of the number 1. Secondly, the myFunc function is invoked. It received the arguments of..." 
+// myFunc take a nummber and a callback funciion arguments. the number muiltply the newNume  and passed it as an argument in the callback function and invoke the callback function.
+
+// call the myFunc function with a number of 10 and an implicit function to display the argument passed in the callback function on the console.
+
+
 
 // 21.
-
 // You might remember the .times method in ruby
 
 // You could do something like this
 
-// 5.times do 
-  // code
+// 5.times do
+// code
 // end
 
 // Define a function times() that takes a number and a callback as an argument, the number represents how many times a loop should run, and the callback is the code that is executed each time the loop runs
@@ -229,8 +244,14 @@ sample.end();
 
 // You should see 5 outputs
 
-// 22. 
+function times(num, cb) {
+  for (let i = 1; i <= num; i++)
+    cb();
+}
+times(5,()=> {console.log('callback')})
 
+
+// 22.
 // Define a Person class, the constructor should take name as an argument and set the name to the this, the class should have a prototype method sayHi() that simply outputs console.log("hello")
 
 // Implement another prototype method addAgeAndHeight() for your class that takes in age and height (in cms) as arguments (both number type) and adds these arguments as attributes to your person object
@@ -243,7 +264,7 @@ sample.end();
 
 // console.log the updated person object showing all three attributes (name, age, height) as being a part of the person object
 
-// 23. 
+// 23.
 
 // Define a function named waitBeforeSum that takes 2 numbers as arguments. Your function should sum these numbers together but only after waiting for 4 seconds inside of a setTimeout.
 
@@ -255,18 +276,18 @@ sample.end();
 
 // Add a try and catch block to your accessSum function, make it go into the catch when the sum is greater than 10, when you console.log the the error that you get as a parameter in the catch it should say 'the sum was greater than 10'
 
-// 24. 
+// 24.
 
-// a. 
+// a.
 // npm install node-fetch
 
-// b. 
+// b.
 // Using the following API endpoint access the Australian flag svg link and console.log it
 // => https://restcountries.eu/rest/v2/all
 
 // c.
-// Using the following API endpoint console.log the yoda text generated from this english phrase "Master Obiwan has lost a planet" 
-// Hint: you don't need to pay for a subscription 
+// Using the following API endpoint console.log the yoda text generated from this english phrase "Master Obiwan has lost a planet"
+// Hint: you don't need to pay for a subscription
 // => https://funtranslations.com/api/yoda
 
 // 🎉🎉🎉🎉🎉🎉
